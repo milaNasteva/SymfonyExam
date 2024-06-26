3582449d656a8fbd539ac6dc3a468e8e3de7b29a
milaNasteva <mila.nasteva@gmial.com>
2024-06-26 11:56:51 +0200

    Added UserFormType with basic validation and integrated into UserController

---
+++ src/Controller/UserController.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace App\Controller;
+
+use App\Form\UserFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user/create", name="user_new")
+     */
+    public function newUser(Request $request): Response
+    {
+        $form = $this->createForm(UserFormType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $formData = $form->getData();
+
+            $this->addFlash('success', 'Form submitted successfully!');
+
+            return $this->redirectToRoute('user_new');
+        }
+
+        return $this->render('user/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
+
+?>
